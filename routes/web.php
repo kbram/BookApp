18ee6e3d277c74f32e341bdde7bd28ed06f9c233
Bavaram <nkbram95@gmail.com>
2020-12-03 19:12:01 +0530

    align

--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,6 @@ Route::get('/home', function () {
     return view('welcome');
 });
 
-
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
 })->name('dashboard');
