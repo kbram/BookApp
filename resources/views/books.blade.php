2c629c97783fdbd93481feb5a9e443c563f8f43a
Bavaram <nkbram95@gmail.com>
2020-12-03 23:50:12 +0530

    book and payments show interfaces

--- resources/views/books.blade.php
+++ resources/views/books.blade.php
@@ -1,15 +1,105 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Books') }}
-        </h2>
-        
+        <div class="row">
+            <div class="col">
+            <h1 class="font-semibold text-xl text-gray-800 leading-tight pt-2">
+                {{ __('Books') }}
+            </h1>
+            </div>
+            <div class="col">
+                <button type="button" class="btn btn-outline-success shadow rounded pull-right">New Book</button>
+            </div>
+        </div>
     </x-slot>
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                <x-jet-welcome />
+            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg p-4">
+                <div class="row p-2">
+                    <div class="column p-4">
+                        <div class="card">
+                            <img src="images/preview/1.jpg" alt="Avatar" style="width:100%">
+                            <div class="container">
+                                <h4><b>John Doe</b></h4> 
+                                <p>Architect & Engineer</p> 
+                            </div>
+                        </div>
+                    </div>
+                    <div class="column p-4">
+                        <div class="card">
+                            <img src="images/preview/2.jpeg" alt="Avatar" style="width:100%">
+                            <div class="container">
+                                <h4><b>John Doe</b></h4> 
+                                <p>Architect & Engineer</p> 
+                            </div>
+                        </div>
+                    </div><div class="column p-4">
+                        <div class="card">
+                            <img src="images/preview/3.png" alt="Avatar" style="width:100%">
+                            <div class="container">
+                                <h4><b>John Doe</b></h4> 
+                                <p>Architect & Engineer</p> 
+                            </div>
+                        </div>
+                    </div><div class="column p-4">
+                        <div class="card">
+                            <img src="images/preview/4.jpg" alt="Avatar" style="width:100%">
+                            <div class="container">
+                                <h4><b>John Doe</b></h4> 
+                                <p>Architect & Engineer</p> 
+                            </div>
+                        </div>
+                    </div><div class="column p-4">
+                        <div class="card">
+                            <img src="images/preview/5.jpeg" alt="Avatar" style="width:100%">
+                            <div class="container">
+                                <h4><b>John Doe</b></h4> 
+                                <p>Architect & Engineer</p> 
+                            </div>
+                        </div>
+                    </div><div class="column p-4">
+                        <div class="card">
+                            <img src="images/preview/6.jpg" alt="Avatar" style="width:100%">
+                            <div class="container">
+                                <h4><b>John Doe</b></h4> 
+                                <p>Architect & Engineer</p> 
+                            </div>
+                        </div>
+                    </div><div class="column p-4">
+                        <div class="card">
+                            <img src="images/preview/7.jpg" alt="Avatar" style="width:100%">
+                            <div class="container">
+                                <h4><b>John Doe</b></h4> 
+                                <p>Architect & Engineer</p> 
+                            </div>
+                        </div>
+                    </div><div class="column p-4">
+                        <div class="card">
+                            <img src="images/preview/8.jpg" alt="Avatar" style="width:100%">
+                            <div class="container">
+                                <h4><b>John Doe</b></h4> 
+                                <p>Architect & Engineer</p> 
+                            </div>
+                        </div>
+                    </div><div class="column p-4">
+                        <div class="card">
+                            <img src="images/preview/10.jpeg" alt="Avatar" style="width:100%">
+                            <div class="container">
+                                <h4><b>John Doe</b></h4> 
+                                <p>Architect & Engineer</p> 
+                            </div>
+                        </div>
+                    </div><div class="column p-4">
+                        <div class="card">
+                            <img src="images/preview/11.jpg" alt="Avatar" style="width:100%">
+                            <div class="container">
+                                <h4><b>John Doe</b></h4> 
+                                <p>Architect & Engineer</p> 
+                            </div>
+                        </div>
+                    </div>
+                </div>  
+                <!-- <x-jet-welcome /> -->
             </div>
         </div>
     </div>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,7 +10,13 @@
         <!-- Fonts -->
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
 
-        <!-- Styles -->
+		<!-- Bootsrap Styles -->
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+		<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+		<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+		<!-- Styles -->
+		
         <link rel="stylesheet" href="{{ mix('css/app.css') }}">
         <link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,400,600,700,300' rel='stylesheet' type='text/css'>
         <link rel="stylesheet" id="fontawesome-css" href="/slider/css/font-awesome/css/font-awesome.css" type="text/css" media="all">
@@ -18,6 +24,46 @@
         <link rel="stylesheet" id="flexslider-css" href="/slider/css/flexslider.css" type="text/css" media="screen">
         <link rel="stylesheet" id="mainstyle-css" href="/slider/css/style0.css" type="text/css" media="all">
 
+		<style>
+.card {
+  box-shadow: 0 4px 8px 0 rgba(0,0,0,0.3);
+  transition: 0.3s;
+  width: 100%;
+  text-align: center;
+}
+
+.card:hover {
+  box-shadow: 0 8px 16px 0 rgba(0,0,0,0.7);
+}
+
+.container {
+  padding: 2px 16px;
+}
+.column {
+  float: left;
+  width: 25%;
+  padding: 0 10px;
+}
+
+/* Remove extra left and right margins, due to padding */
+.row {margin: 0 -5px;}
+
+/* Clear floats after the columns */
+.row:after {
+  content: "";
+  display: table;
+  clear: both;
+}
+
+/* Responsive columns */
+@media screen and (max-width: 600px) {
+  .column {
+    width: 100%;
+    display: block;
+    margin-bottom: 20px;
+  }
+}
+</style>
         @livewireStyles
 
         <!-- Scripts -->
